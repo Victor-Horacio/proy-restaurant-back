f97efd3c0f837e142f60667c5a0c428c35b1af94
Victor <victor_weazelnews@outlook.com>
2022-06-11 14:04:35 -0500

    Cambios en App.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const history = require('connect-history-api-fallback')
+//const path = require('path');
+//const history = require('connect-history-api-fallback')
+const bodyparser = require('body-parser')
 const cors = require('cors');
 const morgan = require('morgan');
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 300
 
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200
+}
+
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
 app.use(morgan('tiny'));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(cors(corsOptions))
+//app.use(cors());
+//app.use(express.json());
+//app.use(express.urlencoded({extended: true}));
 
 app.use('/meseros',require('./routes/meseros'));
 app.use('/comidaxbebida',require('./routes/comidaxbebida'));
@@ -18,9 +27,8 @@ app.use('/mesa',require('./routes/mesa'));
 app.use('/orden',require('./routes/orden'));
 app.use('/ordenespec',require('./routes/ordenespec'));
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(history());
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use(history());
 
 app.get('/', (req, res) => {
     res.json({
